4e5de060a043a016c606a1e8d4de058050bccd55
Sofia Ma <sofia.ma@gmail.com>
2014-09-25 22:58:45 -0400

    install devise, add a user, and install devise views

--- db/migrate/20140926002732_create_statuses.rb
+++ db/migrate/20140926002732_create_statuses.rb
@@ -1,5 +1,9 @@
 class CreateStatuses < ActiveRecord::Migration
   def change
+  	add_column :status, :user_id, :integer
+  	add_index :statuses, :user_id
+  	remove_column :statuses, :name
+
     create_table :statuses do |t|
       t.string :name
       t.text :content
